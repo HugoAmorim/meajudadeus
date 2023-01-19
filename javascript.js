58fcdc340bff0d5d2fd62b7159ec389ed9a11883
HugoAmorim <naonaosei80@gmail.com>
2023-01-19 18:10:08 -0300

    chorei

--- javascript.js
+++ javascript.js
@@ -5,6 +5,8 @@ const data = {
   ler: ["01-01", "01-02", "01-06"],
   dormi: ["01-04", "01-05"],
   agua: ["01-01", "01-02"],
+  chorei: ["1-3", "1-9"],
+  estudando: ["1-6", "1-3"],
 }
 
 nlwSetup.setData(data)
