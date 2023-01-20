b088ed02a5facc8d4ddb41807c4c1a4eba9428ce
HugoAmorim <naonaosei80@gmail.com>
2023-01-20 12:57:13 -0300

    datas

--- javascript.js
+++ javascript.js
@@ -1,13 +1,27 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  ler: ["01-01", "01-02", "01-06"],
-  dormi: ["01-04", "01-05"],
-  agua: ["01-01", "01-02"],
-  chorei: ["01-03", "01-09"],
-  estudando: ["01-06", "01-03"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Ja foi registrado")
+    return
+  }
+  alert("Adicionado com sucesso")
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem("pepe@", JSON.stringify(nlwSetup.data))
+  localStorage.getItem("pepe@")
+  JSON.parse(localStorage.getItem("pepe@"))
+}
+const data = JSON.parse(localStorage.getItem("pepe@")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
